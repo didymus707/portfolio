da016c0f64174734476c7486b2653052c86a26c9
Adewale Orotayo <didymus7007@gmail.com>
2021-02-17 17:41:19 +0100

    Added git ignore

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const app = express();
 
@@ -7,3 +8,23 @@ const path = require('path');
 const PORT = 8080;
 
 app.get('/')
+=======
+const mailgun = require("mailgun-js");
+const DOMAIN = 'adewale.me';
+const key = apiKey.key;
+const mg = mailgun({apiKey: key, domain: DOMAIN});
+const firstName = document.querySelector('#firstName').value;
+const lastName = document.querySelector('#lastName').value;
+const fullName = document.querySelector('#fullName').value;
+const email = document.querySelector('#email').value;
+const textArea = document.querySelector('#textArea').value;
+const data = {
+	from: `${(firstName + lastName) || (fullName)}  <me@samples.mailgun.org>`,
+	to: 'bar@example.com, YOU@YOUR_DOMAIN_NAME',
+	subject: 'Hello',
+	text: 'Testing some Mailgun awesomness!'
+};
+mg.messages().send(data, function (error, body) {
+	console.log(body);
+});
+>>>>>>> 89723ab (fixes)
