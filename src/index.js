657018f407c159d6b0da53e0001b8627a9940de3
Adewale Orotayo <didymus7007@gmail.com>
2021-02-19 10:48:02 +0100

    Added script to send mail to contact

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const menuIcon = document.querySelector('.menu-icon');
 const overlay = document.querySelector('.overlay');
 const gallery = document.querySelector('.gallery');
+const form = document.querySelector(".form");
+const git = document.querySelector("#getintouch");
+const status = document.querySelector("#status");
 const getScreenWidth = () => window.innerWidth;
+
 const width = getScreenWidth();
 
 const toggleChange = () => {
@@ -38,5 +42,40 @@ window.onclick = e => {
     e.target.style.display = 'none';
   } else if (e.target.className === 'close') {
     e.target.offsetParent.offsetParent.style.display = 'none';
+  }
+}
+
+const success = () => {
+  form.reset();
+  status.classList.add("success");
+  status.innerHTML = 'Thanks!';
+}
+
+const err = () => {
+  status.classList.add("error");
+  status.innerHTML = 'Oops, there was a problem!. Try again in 15minutes!'
+}
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = new FormData(form);
+  console.log(form.action);
+  postForm(form.action, data, success, err);
+})
+
+const postForm = async (link, data, success, error) => {
+  try {
+    const request = await fetch(link, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+      },
+      body: data,
+    });
+    const response = await request
+    response.status === 200 ? success() : error()
+  } catch (err) {
+    throw new Error(error)
   }
 }
